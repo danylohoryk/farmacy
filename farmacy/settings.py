015c25f4b3841ebfb2e8c81f4e57e20b43d173ff
danylohoryk <danylo.horyk@gmail.com>
2021-02-02 20:05:50 +0200

    'Update'

--- farmacy/settings.py
+++ farmacy/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '%m+hympm#!x(&ml!&)+1xagh4$=sbp
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['farmacylviv.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
